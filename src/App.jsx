069eaba95b6350bf5e948fc6072ceeaa50c386b2
moscowmule7 <oguzsamsa@gmail.com>
2024-03-11 19:09:38 +0300

    a few little design updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Hello world!
-      </h1>
+      <div className="bg-[linear-gradient(to_right,_#4731D3_70%,_#CBF281_70%)]  w-full mx-auto">
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Hello world!
+        </h1>
+      </div>
     </>
   );
 }
